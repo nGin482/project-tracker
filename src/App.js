9e0e9877a4b23762443fef9710cd65983ce5a43b
nGin482 <nicholasgiumelli@gmail.com>
2023-05-06 21:16:07 +1000

    Using React Router to handle pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, {useState, useEffect } from "react";
-import { Button } from "antd";
 
-import TaskCard from './components/task-card/TaskCard';
-import NewTask from "./components/new-task/NewTask";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import IndexPage from "./pages/IndexPage";
 import TasksContext from "./contexts/TasksContext";
 import { getTasks } from "./services/requests";
 import './App.css';
+import TaskPage from "./pages/TaskPage";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     getTasks().then(tasks => {
@@ -17,17 +17,21 @@ function App() {
     })
   }, []);
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <IndexPage />
+    },
+    {
+      path: '/task/:taskID',
+      element: <TaskPage />
+    }
+  ]);
+
   return (
 
     <TasksContext.Provider value={{tasks, setTasks}}>
-      <h1>Project Tracker</h1>
-      <div id="container">
-        <Button id="create-task" onClick={() => setShowForm(true)}>Create New Task</Button>
-        {tasks.map(task => (
-          <TaskCard key={task.id} task={task} />
-        ))}
-      </div>
-      <NewTask showForm={showForm} setShowForm={setShowForm} project="DVD-Library"/>
+      <RouterProvider router={router} />
     </TasksContext.Provider>
   );
 }
