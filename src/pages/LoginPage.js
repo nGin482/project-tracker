d9b7412e0b21b6c6106954b11c75bb60b9d86d0e
nGin482 <nicholasgiumelli@gmail.com>
2023-05-20 15:43:41 +1000

    Remove unused import

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import Registration from "../components/registration/Registration";
 import Login from "../components/login/Login";
