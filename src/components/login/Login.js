05b16069014b61c61d15df0450bbacff19a470e8
nGin482 <nicholasgiumelli@gmail.com>
2023-05-20 13:13:49 +1000

    Login component

---
+++ src/components/login/Login.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import { Alert, Button, Form, Input } from "antd";
+
+import { login } from "../../services/requests";
+import "./Login.css";
+
+const Login = props => {
+    const [form] = Form.useForm();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const loginUser = () => {
+        form.validateFields().then(values => {
+            const { username, password } = values;
+            login(username, password).then(data => {
+                setErrorMessage('');
+                console.log('redirecting to login page', data)
+            }).catch(err => {
+                setErrorMessage(err.response.data);
+            })
+        }).catch(err => {
+            setErrorMessage(err);
+        })
+    }
+
+
+    return (
+        <Form
+            form={form}
+            id="login-form"
+            onFinish={loginUser}
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please provide your username."
+                    }
+                ]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please provide your password."
+                    }
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+            {errorMessage !== '' ? (
+                <Alert 
+                    type="error"
+                    message={errorMessage}
+                    showIcon
+                    className="login-alert"
+                 /> 
+                )
+            : ''}
+            <Form.Item>
+                <Button
+                    type="primary"
+                    htmlType="submit"
+                    id="login-submit"
+                >
+                    Login
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default Login;
