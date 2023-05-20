4ede59759de524d8e3cd0082b6e0d56205650a18
nGin482 <nicholasgiumelli@gmail.com>
2023-05-20 16:30:38 +1000

    Only show login button if user is undefined

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { Button, Layout } from "antd";
 import { LoginOutlined } from "@ant-design/icons";
 
 import NewTask from "../components/new-task/NewTask";
+import UserContext from "../contexts/UserContext";
 import logo from "../assets/Logo.png";
 import "./Navbar.css";
 
@@ -12,6 +13,7 @@ const Navbar = props => {
     const { Header } = Layout;
 
     const [showForm, setShowForm] = useState(false);
+    const { user } = useContext(UserContext);
 
     return (
         <Header className="navbar">
@@ -21,11 +23,17 @@ const Navbar = props => {
             </span>
             <Button id="create-task-button" onClick={() => setShowForm(true)}>Create New Task</Button>
             <NewTask showForm={showForm} setShowForm={setShowForm} project="DVD-Library"/>
-            <Button id="login-button">
-                <NavLink to='/login'>
-                    <LoginOutlined />Login
-                </NavLink>
-            </Button>
+            {user ? (
+                <span>{user.username}</span>
+            ) : (
+                <Button id="login-button">
+                    <NavLink to='/login'>
+                        <LoginOutlined />Login
+                    </NavLink>
+                </Button>
+            )}
+
+            
         </Header>
     );
 };
