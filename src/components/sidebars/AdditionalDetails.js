1c8eab8f1463acae13c882d159f6c3f09d3a9cd2
nGin482 <nicholasgiumelli@gmail.com>
2023-06-04 00:23:14 +1000

    Moving Tasks by Project list into a Drawer

--- src/components/sidebars/AdditionalDetails.js
+++ src/components/sidebars/AdditionalDetails.js
@@ -2,20 +2,22 @@ import { useState, useEffect } from "react";
 import { Space, Tag } from "antd";
 
 import StatusTag from "../status-tag/StatusTag";
-import "./AdditionalDetails.css";
 import Sider from "antd/es/layout/Sider";
+import "./AdditionalDetails.css";
 
 
 const AdditionalDetails = props => {
-    const { status, project, created, showHeader, width } = props;
+    const { taskDetails, showHeader, width } = props;
+    const { status, project, created, creator } = taskDetails;
 
     return (
         <Sider width={width} className="additional-details">
-            {showHeader ? <h4>Task Details</h4> : ''}
+            {showHeader && <h4>Task Details</h4>}
             <Space direction="vertical" size="small">
                 <StatusTag status={status} showDescription={true}/>
                 <span className="project">Project: <Tag color="blue-inverse">{project}</Tag></span>
                 <span className="created">Created: <Tag color="#e57200">{created}</Tag></span>
+                <span className="reporter">Reporter: <Tag>{creator}</Tag></span>
             </Space>
         </Sider>
     );
--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -1,10 +1,9 @@
 import {useState, useEffect, useContext} from "react";
-import { useParams } from "react-router-dom";
-import { Layout, Spin, List } from "antd";
-import { isEmpty } from "lodash";
+import { useParams, NavLink } from "react-router-dom";
+import { Layout, Spin, Drawer, Button, List, Input } from "antd";
+import { isEmpty, omit } from "lodash";
 
 import AdditionalDetails from "../components/sidebars/AdditionalDetails";
-import LeftSidebar from "../components/sidebars/LeftSidebar";
 import Navbar from "../components/Navbar";
 import ErrorPage from "./ErrorPage";
 import ErrorsContext from "../contexts/ErrorsContext";
@@ -13,12 +12,16 @@ import "./styles/TaskPage.css";
 
 
 const TaskPage = () => {
-    const { Content, Sider } = Layout;
-
+    const { Content } = Layout;
     const { taskID } = useParams();
+
     const [task, setTask] = useState({});
     const [tasksByProject, setTasksByProject] = useState([]);
+    const [showTasksDrawer, setShowTasksDrawer] = useState(false);
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
     const [errors, setErrors] = useState(false);
+    
     const { setErrorMessage, setErrorType } = useContext(ErrorsContext);
     
     useEffect(() => {
@@ -26,6 +29,7 @@ const TaskPage = () => {
             setTask(data);
             getTasksByProject(data.project).then(data => {
                 setTasksByProject(data);
+                setSearchResults(data)
             })
         }).catch(err => {
             setErrors(true);
@@ -36,29 +40,64 @@ const TaskPage = () => {
         })
     }, [taskID]);
 
-    const changeStatus = status => {
-        setTask({...task, status: status});
+    useEffect(() => {
+        if (search !== '') {
+            setSearchResults(tasksByProject.filter(task => 
+                task.title.toLowerCase().includes(search.toLowerCase())
+            ));
+        }
+        else {
+            setSearchResults(tasksByProject);
+        }
+    }, [search]);
+
+    useEffect(() => {
+        console.log(task)
+    }, [task])
+
+    const handleSearch = event => {
+        setSearch(event.target.value);
     };
 
-    return  (
+    return (
         <>
             <Navbar />
             {!isEmpty(task) ? (
                 <div id="task-page">
-                    {tasksByProject.length > 0 ? <LeftSidebar listItems={tasksByProject} view="Tasks" /> : ''}
+                    {tasksByProject.length > 0 && (
+                        <Drawer
+                            title={`Tasks in ${task.project}`}
+                            placement="left"
+                            open={showTasksDrawer}
+                            onClose={() => setShowTasksDrawer(!showTasksDrawer)}
+                        >
+                            <Input placeholder="Search" onChange={handleSearch}/>
+                            <List
+                                dataSource={searchResults}
+                                bordered
+                                renderItem={item => (
+                                    <List.Item>
+                                        <NavLink to={`/task/${item.taskID}`}><h3>{item.title}</h3></NavLink>
+                                    </List.Item>
+                                )}
+                            />
+                        </Drawer>
+                    )}
                     <Layout>
                         <div id="task-content">
+                            <Button
+                                onClick={() => setShowTasksDrawer(true)}
+                                id="tasks-drawer-button"
+                            >
+                                Show Tasks in this Project
+                            </Button>
                             <Content>
                                 <h1>{task.title}</h1>
                                 <p>{task.description}</p>
                             </Content>
                         </div>
-
                         <AdditionalDetails
-                            status={task.status}
-                            changeStatus={changeStatus}
-                            project={task.project}
-                            created={task.created}
+                            taskDetails={omit(task, 'title', 'description', 'taskID', 'comments')}
                             showHeader
                             width={470}
                         />
