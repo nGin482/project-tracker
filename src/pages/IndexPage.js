6c7fe5ef0cda655ad3011196f261b1406187bd19
nGin482 <nicholasgiumelli@gmail.com>
2023-06-09 18:09:15 +1000

    Drawer component to view & search list of projects

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,17 +1,60 @@
 import {useState, useEffect, useContext} from "react";
+import { Button, Drawer, Input, List } from "antd";
 
 import TaskCard from '../components/task-card/TaskCard';
 import TasksContext from "../contexts/TasksContext";
 import Navbar from "../components/navbar/Navbar";
 
 
-const IndexPage = () => {
+const IndexPage = props => {
+    const { projects } = props;
     const { tasks } = useContext(TasksContext);
+
+    const [showProjects, setShowProjects] = useState(false);
+    const [searchResults, setSearchResults] = useState(projects);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        if (search !== '') {
+            setSearchResults(projects.filter(project => 
+                project.projectName.toLowerCase().includes(search.toLowerCase())
+            ));
+        }
+        else {
+            setSearchResults(projects);
+        }
+    }, [search]);
+
+    useEffect(() => {
+        setSearchResults(projects);
+    }, [projects])
+
+    const handleSearch = event => {
+        setSearch(event.target.value);
+    };
 
 
     return  (
         <>
             <Navbar />
+            <Button onClick={() => setShowProjects(!showProjects)}>Find Project</Button>
+            <Drawer
+                title='Projects'
+                placement="left"
+                open={showProjects}
+                onClose={() => setShowProjects(false)}
+            >
+                <Input placeholder="Search" onChange={handleSearch}/>
+                <List
+                    dataSource={searchResults}
+                    bordered
+                    renderItem={item => (
+                        <List.Item>
+                            <h3>{item.projectCode}: {item.projectName}</h3>
+                        </List.Item>
+                    )}
+                />
+            </Drawer>
             <div id="container">
                 {tasks.map(task => (
                     <TaskCard key={task.taskID} task={task} />
