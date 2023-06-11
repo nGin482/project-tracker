746c3cb4860aefa18806c39d176063647aa86329
nGin482 <nicholasgiumelli@gmail.com>
2023-06-11 12:40:28 +1000

    Don't update tag and show a modal if the update task status request fails

--- src/components/status-tag/StatusTag.js
+++ src/components/status-tag/StatusTag.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Tag, Dropdown } from "antd";
+import { Tag, Dropdown, Modal } from "antd";
 
 import useTaskStatus from "../../hooks/useTaskStatus";
 
@@ -8,6 +8,8 @@ const StatusTag = props => {
     const { status, taskID, showDescription } = props;
     const [colour, setColour] = useState('#A0A0A0');
     const { taskStatus, changeStatus } = useTaskStatus(status);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const items = [
         {
@@ -53,14 +55,24 @@ const StatusTag = props => {
     }, [taskStatus]);
 
     const onClick = event => {
-        changeStatus(taskID, event.domEvent.target.innerText);
-    }
+        changeStatus(taskID, event.domEvent.target.innerText).catch(err => {
+            setError(true);
+            if (err.response.data) {
+                setErrorMessage(err.response.data);
+            }
+        });
+    };
 
     return (
         <div className="task-status">
             {showDescription && "Status: "}<Dropdown menu={{items,onClick,}} trigger={['click']}>
                 <Tag color={colour}>{taskStatus}</Tag>
             </Dropdown>
+            {error && (
+                <Modal open={error} onOk={() => setError(false)} onCancel={() => setError(false)}>
+                    <p>{errorMessage}</p>
+                </Modal>
+            )}
         </div>
     );
 }
--- src/hooks/useTaskStatus.js
+++ src/hooks/useTaskStatus.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
+import UserContext from "../contexts/UserContext";
 import { updateTask } from "../services/requests";
 
 const useTaskStatus = status => {
     const [taskStatus, setTaskStatus] = useState(status);
+    const { user } = useContext(UserContext);
 
     const changeStatus = (taskID, newStatus) => {
-        setTaskStatus(newStatus);
-        updateTask(taskID, {field: 'status', value: newStatus});
+        return updateTask(taskID, {field: 'status', value: newStatus}, user.token).then(() => {
+            setTaskStatus(newStatus);
+            return Promise.resolve();
+        }).catch(err => {
+            return Promise.reject(err);
+        });
     };
 
     return {
