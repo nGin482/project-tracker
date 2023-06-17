946ab7a4a741e191fd9d65e607a7f510778f7821
nGin482 <nicholasgiumelli@gmail.com>
2023-06-17 12:46:08 +1000

    Component to link tasks

---
+++ src/components/link-tasks/LinkTasks.js
@@ -0,0 +1,122 @@
+import { useState, useContext } from "react";
+import { Alert, AutoComplete, Button, Modal } from "antd";
+import { linkTasks } from "../../services/requests";
+
+import UserContext from "../../contexts/UserContext";
+
+const LinkTasks = props => {
+    const { taskID, tasksByProject, setVisible } = props;
+    const { user } = useContext(UserContext);
+
+    const [linkedTasks, setLinkedTasks] = useState([]);
+    const [linkTaskInput, setLinkTaskInput] = useState('');
+    const [linkTasksDropdownVisibile, setLinkTasksDropdownVisibile] = useState(false);
+    const [linkTasksDropdownOptions, setLinkTasksDropdownOptions] = useState(false);
+    
+    // Errors
+    const [showErrorModal, setShowErrorModal] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const linkSearchTasks = searchTerm => {
+        setLinkTasksDropdownVisibile(true);
+        setLinkTaskInput(searchTerm);
+        if (searchTerm !== '') {
+            setLinkTasksDropdownOptions(
+                tasksByProject.filter(task => 
+                    task.title.toLowerCase().includes(searchTerm) ||
+                    task.taskID.toLowerCase().includes(searchTerm)
+                )
+                .map(task => ({value: `${task.taskID}: ${task.title}`}))
+            );
+        }
+        else {
+            setLinkTasksDropdownVisibile(false);
+        }
+    };
+
+    const selectLinkedTask = item => {
+        setLinkTasksDropdownVisibile(false);
+        setLinkedTasks([...linkedTasks, item]);
+        setLinkTaskInput('');
+    };
+    
+    const removeLinkTask = task => {
+        setLinkedTasks(linkedTasks.filter(linkTask => linkTask !== task));
+    }
+
+    const cancelLinkingTask = () => {
+        setLinkTaskInput('');
+        setLinkTasksDropdownVisibile(false);
+        setLinkedTasks([]);
+        setVisible(false);
+    }
+    
+    const confirmLinkTasks = () => {
+        const tasksToLink = linkedTasks.map(task => task.split(': ')[0])
+        linkTasks(taskID, {linkedTasks: tasksToLink}, user.token).then(() => {
+            setLinkedTasks([]);
+        })
+        .catch(err => {
+            setShowErrorModal(true)
+            err.response.data ? setErrorMessage(err.response.data) : setErrorMessage(err);
+        })
+    }
+
+    const closeFailureModal = () => {
+        setShowErrorModal(false);
+        setErrorMessage('');
+    }
+
+    return (
+        <>
+            <div id="link-task-input">
+                <AutoComplete
+                    className="link-task-search"
+                    onSearch={text => linkSearchTasks(text)}
+                    onSelect={item => selectLinkedTask(item)}
+                    open={linkTasksDropdownVisibile}
+                    options={linkTasksDropdownOptions}
+                    placeholder="Search for Task to Link"
+                    value={linkTaskInput}
+                />
+                {linkedTasks.length > 0 && (    
+                    <div id="confirm-cancel-link">
+                        <Button
+                            type="primary"
+                            onClick={() => confirmLinkTasks()}
+                        >
+                            Confirm
+                        </Button>
+                        <Button
+                            type="default"
+                            className="cancel-link-tasks"
+                            onClick={cancelLinkingTask}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                )}
+            </div>
+            {linkedTasks.map((task, i) => (
+                <div className="link-task" key={`link-task-${i}`}>
+                    <span>{task}</span>
+                    <Button
+                        onClick={() => removeLinkTask(task)}
+                        className="remove-link-task"
+                    >
+                        Remove
+                    </Button>
+                </div>
+            ))}
+            <Modal
+                open={showErrorModal}
+                onCancel={cancelLinkingTask}
+                onOk={closeFailureModal}
+            >
+                <Alert type="error" message={errorMessage} showIcon />
+            </Modal>
+        </> 
+    )
+};
+
+export default LinkTasks;
