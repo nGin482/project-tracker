49a8bede46412ca362251b06354f927cfd2691b1
nGin482 <nicholasgiumelli@gmail.com>
2023-06-27 16:33:49 +1000

    Delete a task from the TaskPage

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,29 +1,61 @@
-import React, {useState, useEffect } from "react";
+import React, {useState, useEffect, useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { Card, Divider } from "antd";
-import { EditOutlined, SettingOutlined } from "@ant-design/icons";
+import { Card, Divider, message, Popconfirm } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 
 import AdditionalDetails from "../sidebars/AdditionalDetails";
 import TaskType from "../task-type-icon/TaskType";
+import useProjects from "../../hooks/useProjects";
+import UserContext from "../../contexts/UserContext";
+import { deleteTask } from "../../services/requests";
 import "./TaskCard.css";
 
 const TaskCard = props => {
     const [task, setTask] = useState(props.task);
-    const { title, description, comments, type } = task;
+    const { title, description, comments, type, project } = task;
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const { deleteTaskState } = useProjects();
+    const { user } = useContext(UserContext);
 
     useEffect(() => {
         console.log(task)
         // update the DB
     }, [task]);
+
+    const handleDeleteTask = () => {
+        deleteTask(task.taskID, user.token).then(data => {
+            deleteTaskState(project, task.taskID);
+        }).catch(err => {
+            if (err.response.data) {
+                messageApi.error(err.response.data);
+            }
+            else {
+                messageApi.error(err);
+            }
+        });
+    };
     
     
     return (
         <Card
             title={<div><TaskType type={type} /><span className={`task-title task-title__${type}`}>{title}</span></div>}
             className="task-card"
-            extra={<SettingOutlined />} 
-            actions={[<NavLink to={`/task/${task.taskID}`}>View {task.taskID}</NavLink>, <EditOutlined key="edit" />]}
+            extra={
+                <Popconfirm
+                    title={`Delete ${task.taskID}`}
+                    description="Are you sure you want to delete this task?"
+                    onConfirm={handleDeleteTask}
+                    okText="Yes"
+                    cancelText="No"
+                >
+                    <DeleteOutlined />
+                </Popconfirm>
+            }
+            actions={[<NavLink to={`/task/${task.taskID}`}>View {task.taskID}</NavLink>]}
         >
+            {contextHolder}
+
             <p>{description}</p>
             <AdditionalDetails
                 taskDetails={task}
