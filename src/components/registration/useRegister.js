3a08cbaa7c99b976b591aa6be03379da65ac6d3d
nGin482 <nicholasgiumelli@gmail.com>
2023-08-03 14:06:06 +1000

    Logic to handle file uploads in Registration component

---
+++ src/components/registration/useRegister.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { message } from "antd";
+import FormData from "form-data";
+
+import { uploadAvatar } from "../../services/requests";
+
+const useRegister = () => {
+    const [messageApi, contextHandler] = message.useMessage();
+    const [avatarURL, setAvatarURL] = useState('');
+
+    const onChange = info => {
+        if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+           console.log(`${info.file.name} file uploaded successfully`);
+        }
+        else if (info.file.status === 'error') {
+            console.log(`${info.file.name} file upload failed.`);
+        }
+    };
+    
+    const uploadFile = event => {
+        const { onSuccess, onError } = event;
+        const formData = new FormData();
+        formData.append('avatar', event.file);
+        uploadAvatar(formData).then(data => {
+            onSuccess(event.file);
+            messageApi.success('File uploaded successfully');
+            setAvatarURL(data);
+        })
+        .catch(err => {
+            const error = err?.response.data ?
+                err.response.data?.error ?
+                err.response.data.error :
+                err.response.data :
+                err;
+            messageApi.error(error);
+            onError(event.file);
+        });
+    };
+
+    return {
+        onChange,
+        uploadFile,
+        contextHandler,
+        avatarURL
+    };
+};
+
+export default useRegister;
