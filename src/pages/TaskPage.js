b4baf867c173e82d482828e95d81b74f2f7c8553
nGin482 <nicholasgiumelli@gmail.com>
2023-08-13 14:04:20 +1000

    Remove setState func

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -67,7 +67,6 @@ const TaskPage = () => {
             });
             getTasksByProject(data.project).then(data => {
                 setTasksByProject(data);
-                setSearchResults(data)
             })
         }).catch(err => {
             setErrors(true);
