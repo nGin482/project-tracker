ac7bc4764779c82db85d8c8a5f32652decaaa0e7
nGin482 <nicholasgiumelli@gmail.com>
2023-08-13 17:41:55 +1000

    Improvments to setting TaskID Return commentID if there aren't any previous

--- utilities/task_utils.js
+++ utilities/task_utils.js
@@ -1,25 +1,26 @@
 const lodash = require("lodash");
 
 const setTaskID = (projectCode, currentTaskIDs) => {
-    let lastTaskID = lodash.last(currentTaskIDs);
-    if (lastTaskID) {
-        lastTaskID = Number(lastTaskID.slice(lastTaskID.indexOf('-')+1));
-        const newTaskID = lastTaskID + 1;
-        return `${projectCode}-${newTaskID}`;
-    }
-    else {
+    if (currentTaskIDs.length === 0) {
         return `${projectCode}-1`;
     }
-}
+    let lastTaskID = lodash.last(currentTaskIDs);
+    lastTaskID = Number(lastTaskID.slice(lastTaskID.indexOf('-')+1));
+    const newTaskID = lastTaskID + 1;
+    return `${projectCode}-${newTaskID}`;
+};
 
 const setCommentID = currentComments => {
+    if (currentComments.length === 0) {
+        return `comment-1`;
+    }
     const latestComment = lodash.last(currentComments);
     const latestCommentID = latestComment.commentID;
     let commentID = Number(latestCommentID.slice(latestCommentID.indexOf('-')+1));
-    commentID = ++commentID;
+    commentID = commentID + 1;
 
     return `comment-${commentID}`;
-}
+};
 
 
 
